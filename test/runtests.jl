1134918790e8e508d56ed0d39dbd24e6567a23d6
Avik Pal <avikpal@mit.edu>
2023-06-26 16:59:53 -0400

    Use safetestsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,7 @@
-include("linear.jl")
+using SafeTestsets, Test
+
+@testset "Complementarity Solve" begin
+    @safetestset "Linear Complementarity Problems" begin
+        include("linear.jl")
+    end
+end
