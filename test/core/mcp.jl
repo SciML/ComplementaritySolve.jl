fcd7484041501e4b0800f5eb03198f53cf54c7ce
Avik Pal <avikpal@mit.edu>
2023-07-07 00:04:37 -0400

    Start testing

---
+++ test/core/mcp.jl
@@ -0,0 +1,8 @@
+using ComplementaritySolve,
+    ComponentArrays, ForwardDiff, SimpleNonlinearSolve, StableRNGs, Test, Zygote
+
+
+@testset "MCPs" begin
+    @testset "Basic MCPs" begin
+    end
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,16 @@
 using SafeTestsets, Test
 
 @testset "Complementarity Solve" begin
-    @safetestset "Linear Complementarity Problems" begin
-        include("linear.jl")
+    @testset "Complementarity Problems" begin
+        @safetestset "Linear Complementarity Problems" begin
+            include("core/lcp.jl")
+        end
+        @safetestset "Mixed Complementarity Problems" begin
+            include("core/mcp.jl")
+        end
     end
 
     @safetestset "Aqua Quality Assurance" begin
-        include("aqua.jl")
+        include("core/aqua.jl")
     end
 end
