b4d6c432d24570ad4878907bb5d7d59ccffc544b
Avik Pal <avikpal@mit.edu>
2023-07-17 17:04:36 -0400

    Update tests

--- test/core/lcp.jl
+++ test/core/lcp.jl
@@ -18,32 +18,32 @@ rng = StableRNG(0)
         prob = LinearComplementarityProblem(A, q, zeros(2))
 
         @testset "solver: $(nameof(typeof(solver)))" for solver in [
-            BokhovenIterativeLCPAlgorithm(),
+            BokhovenIterativeAlgorithm(),
             RPSOR(; ω=1.0, ρ=0.1),
             PGS(),
-            InfeasibleInteriorPointMethod(),
         ]
             sol = solve(prob, solver)
 
             @test sol.u≈[4.0 / 3, 7.0 / 3] rtol=1e-3
-            @test sol.w≈[0.0, 0.0] atol=1e-6
+            w = A * sol.u .+ q
+            @test w≈[0.0, 0.0] atol=1e-6
         end
 
         @testset "Batched Version" begin
             prob = LinearComplementarityProblem(A, q, rand(rng, 2, 4))
 
             @testset "solver: $(nameof(typeof(solver)))" for solver in [
-                BokhovenIterativeLCPAlgorithm(),
+                BokhovenIterativeAlgorithm(),
                 RPGS(),
                 PGS(),
                 PSOR(),
                 NonlinearReformulation(:smooth, SimpleDFSane(; batched=true)),
-                InfeasibleInteriorPointMethod(),
             ]
                 sol = solve(prob, solver)
 
                 @test all(z -> ≈(z, [4.0 / 3, 7.0 / 3]; rtol=1e-3), eachcol(sol.u))
-                @test all(w -> ≈(w, [0.0, 0.0]; atol=1e-3), eachcol(sol.w))
+                @test all(((i, z),) -> ≈(A[:, :, i] * z .+ q[:, i], [0.0, 0.0]; atol=1e-3),
+                    enumerate(eachcol(sol.z)))
             end
         end
 
