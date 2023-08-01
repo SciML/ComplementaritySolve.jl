3d155d1aae06c9b4fedd2a01c32c721f628a25ab
Avik Pal <avikpal@mit.edu>
2023-08-01 17:14:12 -0400

    Streamline testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,24 @@
 using SafeTestsets, Test
 
 const BACKEND_GROUP = uppercase(get(ENV, "BACKEND_GROUP", "All"))
+const TESTING_GROUP = uppercase(get(ENV, "TESTING_GROUP", "All"))
+
+macro testif(group, val, expr)
+    return quote
+        if ($(esc(group)) == "ALL") || $(esc(group)) == uppercase($(esc(val)))
+            @testset "$(g)" for g in ($(esc(group)),)
+                $(esc(expr))
+            end
+        end
+    end
+end
+
+@info "Running tests for $(BACKEND_GROUP) backend"
+@info "Running tests for $(TESTING_GROUP) group"
 
 @testset "Complementarity Solve" begin
-    @testset "Core" begin
-        if BACKEND_GROUP == "ALL" || BACKEND_GROUP == "CPU"
+    @testif TESTING_GROUP "Core" begin
+        @testif BACKEND_GROUP "CPU" begin
             @safetestset "Linear Complementarity Problems" begin
                 include("core/lcp.jl")
             end
@@ -14,8 +28,8 @@ const BACKEND_GROUP = uppercase(get(ENV, "BACKEND_GROUP", "All"))
         end
     end
 
-    @testset "Applications" begin
-        if BACKEND_GROUP == "ALL" || BACKEND_GROUP == "CPU"
+    @testif TESTING_GROUP "Applications" begin
+        @testif BACKEND_GROUP "CPU" begin
             @testset "Differentiable Controller Learning" begin
                 @safetestset "Acrobot with Soft Joint Limits" begin
                     include("applications/control_learning/soft_joint_acrobot.jl")
@@ -30,9 +44,11 @@ const BACKEND_GROUP = uppercase(get(ENV, "BACKEND_GROUP", "All"))
         end
     end
 
-    if BACKEND_GROUP == "ALL" || BACKEND_GROUP == "CPU"
-        @safetestset "Aqua Quality Assurance" begin
-            include("core/aqua.jl")
+    @testif TESTING_GROUP "QA" begin
+        @testif BACKEND_GROUP "CPU" begin
+            @safetestset "Aqua Quality Assurance" begin
+                include("core/aqua.jl")
+            end
         end
     end
 end
