1a83810806dc270bb7275ed58637a8df7d721d66
yonatanwesen <yonatanwesenyeleh1@gmail.com>
2023-08-09 15:29:59 -0400

    create sparse pattern once

--- src/algorithms/mcp/nonlinear_reformulation.jl
+++ src/algorithms/mcp/nonlinear_reformulation.jl
@@ -2,21 +2,23 @@ for method in (:minmax, :smooth)
     algType = NonlinearReformulation{method}
     op = Symbol("$(method)_transform")
     @eval function __solve(prob::MCP{true}, alg::$algType, u0, p; kwargs...)
+        
         function f!(residual, u, θ)
             prob.f(residual, u, θ)
             residual .= $(op).(residual, u, prob.lb, prob.ub)
             return residual
         end
 
+        residual = similar(u0)
+        J0= Float64.(Symbolics.jacobian_sparsity(f!,residual,u0,p))
+        colors = SparseDiffTools.matrix_colors(J0)
+
         function jac!(J,u,θ)
             function _f!(F,u)
                 f!(F,u,θ)
                 return F
             end
-            residual = similar(u)
-            jac = Float64.(Symbolics.jacobian_sparsity(f!,residual,u,θ))
-            colors = SparseDiffTools.matrix_colors(jac)
-            FiniteDiff.finite_difference_jacobian!(J,_f!,u,colorvec=colors,sparsity=jac)
+            FiniteDiff.finite_difference_jacobian!(J,_f!,u,colorvec=colors,sparsity=J0)
         end
 
         _prob = NonlinearProblem(NonlinearFunction{true}(f!;jac=jac!), u0, p)
@@ -27,19 +29,20 @@ for method in (:minmax, :smooth)
 
     @eval function __solve(prob::MCP{false}, alg::$algType, u0, p; kwargs...)
         f(u, θ) = $(op).(prob.f(u, θ), u, prob.lb, prob.ub)
-        
-        function jac(u,θ)
-            function _f!(F,u,θ)
+        function _f!(F,u,θ)
                 F .= f(u,θ)
                 return F
-            end
-            residual = similar(u)
-            jac = Float64.(Symbolics.jacobian_sparsity(_f!,residual,u,θ))
+        end
+        
+        residual = similar(u0)
+        J0 = Float64.(Symbolics.jacobian_sparsity(_f!,residual,u0,p))
+        colors = SparseDiffTools.matrix_colors(J0)
+        
+        function jac(u,θ)   
             function _f(u)
                 return f(u,θ)
             end
-            colors = SparseDiffTools.matrix_colors(jac)
-            return FiniteDiff.finite_difference_jacobian(_f,u,colorvec=colors,sparsity=jac)
+            return FiniteDiff.finite_difference_jacobian(_f,u,colorvec=colors,sparsity=J0)
         end
 
         _prob = NonlinearProblem(NonlinearFunction{false}(f;jac = jac), u0, p)
